8dafead71e1449fb092ca15579cdbb3a113c1042
Luke Edwards <luke.edwards05@gmail.com>
2016-08-11 21:48:34 -0700

    add todo

--- lib/backfeed.ex
+++ lib/backfeed.ex
@@ -13,6 +13,7 @@ defmodule Backfeed do
   def process!(url) when is_binary(url), do: handler(lookup(url), &feed(&1, :minimal))
   def process!(url) when is_list(url), do: handler(url, &feed(&1, :minimal))
 
+  # @todo: Task.await/async iteration
   defp handler(list, func), do: Enum.map(list, func) |> List.flatten |> Enum.uniq
 
   @spec open(String.t) :: map()
