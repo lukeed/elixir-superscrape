b02df2d796e6b04e8bc29e357766331fed884413
Luke Edwards <luke.edwards05@gmail.com>
2016-08-12 01:06:19 -0800

    add package meta

--- mix.exs
+++ mix.exs
@@ -1,11 +1,14 @@
 defmodule SuperScrape.Mixfile do
   use Mix.Project
 
+  @desc "Read an Atom/RSS feed and concatenate it with deduplicated historical items from past versions saved in the Wayback Machine."
+
   def project do
     [app: :superscrape,
      version: "0.1.0",
-     elixir: "~> 1.3",
-     description: "Read an Atom/RSS feed and concatenate it with deduplicated historical items from past versions saved in the Wayback Machine.",
+     elixir: "~> 1.2",
+     description: @desc,
+     package: package,
      deps: deps]
   end
 
@@ -21,5 +24,12 @@ defmodule SuperScrape.Mixfile do
     [{:wayback, "~> 0.1.0"},
      {:scrape, "~> 1.2"},
      {:ex_doc, "~> 0.13.0", only: :dev}]
+  end
+
+  def package do
+    [name: :superscrape,
+     maintainers: ["Luke Edwards"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/lukeed/elixir-superscrape"}]
   end
 end
